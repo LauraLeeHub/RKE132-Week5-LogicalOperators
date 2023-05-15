5b671dcfbd1804f70de3bcc0e4f518ac165683d9
Laura Ebber <lauraebber@gmail.com>
2023-05-15 16:59:03 +0300

    create new app

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+//AND - /&&
+
+//true && --> true
+//true && --> false
+//false && true //> false
+//fa;se && false --> false
+
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
